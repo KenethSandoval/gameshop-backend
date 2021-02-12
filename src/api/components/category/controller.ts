4d9140446ad707580afc07a6a781951256ef85f4
KenethSandoval <kenethra.74@gmail.com>
2021-02-11 22:35:10 -0600

    fix: Logic to delete the category with the product

--- src/api/components/category/controller.ts
+++ src/api/components/category/controller.ts
@@ -1,11 +1,22 @@
-import { createCategoryStore, listCategoryStore } from './store';
+import { 
+  createCategoryStore, 
+  listCategoryStore, 
+  deleteCategoryStore,
+  updateCategoryStore
+} from './store';
 
-export const createCategory = async (name: string) => {
+export const createCategory = async (name: string = '') => {
 	try {
-		const nameExists = name.toString();
-		if (!nameExists) {
+		name = name.trim().toLowerCase();
+		if (!name) 
 			throw {message: 'Missing data something like name', code: 400}
-		}
+		
+
+    const categoryAlreadyExists = (await listCategoryStore({ name })).pop()
+
+    if (categoryAlreadyExists) 
+      throw {message: `Category with name ${name} already exists`, code: 400}
+    
 
 		return await createCategoryStore(name);
 	} catch (err) {
@@ -20,4 +31,41 @@ export const listCategory = async () => {
 	} catch (err) {
 	  throw err;
 	}
+}
+
+export const updateCategory = async (_id:string, name: string = '') => {
+  try {
+    if (!_id)
+      throw { message: 'You need to send an id', code: 400 }
+     
+    name = name.trim().toLowerCase();
+    
+    if (!name) 
+      throw { message: 'Missing data something like name', code: 400 }
+
+    const categoryAlreadyExists = (await listCategoryStore({name})).pop();
+
+    if (categoryAlreadyExists)
+      throw { message: `Category with ${name} already exists`, code: 400 }
+
+    return await updateCategoryStore(_id, { name }); 
+
+  } catch (err) {
+    throw err; 
+  }
+}
+
+export const deleteCategory = async (_id:string) => {
+  try {
+    if (!_id) {
+      throw {message: 'You need to send an id', code: 400}
+    }
+
+    // Logica para borrar la categoria con el producto
+    
+    return await deleteCategoryStore(_id);
+
+  } catch (err) {
+    throw err;
+  }
 }
